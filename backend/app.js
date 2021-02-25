1d7ae34f4f2c83f9db878e05741245b88b4645f8
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2021-02-25 17:00:04 +0300

    fix: trailing slash

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const options = {
   origin: [
-    // 'http://localhost:3000',
+    'http://localhost:3000',
     'http://annakin.students.nomoreparties.space/',
     'https://annakin.students.nomoreparties.space/',
   ],
